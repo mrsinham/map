1ed74df9e034004f5109fd91fc387a5b5ccd21ea
Julien Lefèvre <julien.lefevr@gmail.com>
2017-06-21 08:21:29 +0200

    saving study

--- sampling_poisson.go
+++ sampling_poisson.go
@@ -1,13 +1,58 @@
 package main
 
-type samplingPoisson struct{
+import (
+	"fmt"
+	"math"
+	"math/rand"
+)
+
+//
+//type samplingPoisson struct {
+//	seed int64
+//	rand *rand.Rand
+//}
+//
+//func NewSamplingPoisson(seed int64) *samplingPoisson {
+//
+//	return &samplingPoisson{
+//		seed: seed,
+//		rand: rand.New(rand.NewSource(seed)),
+//	}
+//}
+//
+//func (s *samplingPoisson) GetPoints(m *Map) {
+//
+//}
+//
+//func (s *samplingPoisson) randomDouble(min, max uint) uint {
+//	return min + uint(uint(s.rand.Int())/(math.MaxInt64/(max-min)))
+//}
+//
+//func (s *samplingPoisson) randomRange(min, max uint) uint {
+//	return min + uint(uint(s.rand.Int())%(max-min))
+//}
+//
+//func (s *samplingPoisson) randomPoint(m *Map) *Point {
+//	return &Point{
+//		X: s.randomDouble(0, m.Limits.X),
+//		Y: s.randomDouble(0, m.Limits.Y),
+//	}
+//}
+//
+//func (s *samplingPoisson) randomDiscPoint(m *Map) *Point {
+//
+//}
+
+type samplingPoisson struct {
 	seed int64
+	rand *rand.Rand
 }
 
 func NewSamplingPoisson(seed int64) *samplingPoisson {
+
 	return &samplingPoisson{
-		seed:seed,
-		rand:math.New(),
+		seed: seed,
+		rand: rand.New(rand.NewSource(seed)),
 	}
 }
 
@@ -15,7 +60,23 @@ func (s *samplingPoisson) GetPoints(m *Map) {
 
 }
 
+// Poisson returns a random number of possion distribution
+func (s *samplingPoisson) Poisson(lambda float64) int64 {
+	if !(lambda > 0.0) {
+		panic(fmt.Sprintf("Invalid lambda: %.2f", lambda))
+	}
+	return s.poisson(lambda)
+}
 
-func (s *samplingPoisson) randomDouble(min, max float32) float32 {
-	return min +
+func (s *samplingPoisson) poisson(lambda float64) int64 {
+	// algorithm given by Knuth
+	L := math.Pow(math.E, -lambda)
+	var k int64 = 0
+	var p float64 = 1.0
+
+	for p > L {
+		k++
+		p *= s.rand.Float64()
+	}
+	return k - 1
 }
